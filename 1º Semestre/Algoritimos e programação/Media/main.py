94e58fb8c626a0f8a185e24ac6d526791f6ddd63
GUILHERME BATISTA DO NASCIMENTO <GBN12300@alesc.sc.gov.br>
2026-03-11 14:54:36 -0300

    Media

--- 1º Semestre/Algoritimos e programação/Media/main.py
+++ 1º Semestre/Algoritimos e programação/Media/main.py
@@ -1,11 +1,17 @@
 Nome_Aluno = input("Digite o nome do aluno: ")
-idade_aluno = int(input("Digite o idade do aluno: "))
-n1 = float(input("Digite a Nota1: "))
-n2 = float(input("Digite a Nota2: "))
-n3 = float(input("Digite a Nota3: "))
-media = (n1 + n2 + n3) / 3
+Idade_Aluno = int(input("Digite a idade do aluno: "))
+
+qtd = int(input("Quantas notas deseja inserir? "))
+
+soma = 0
+
+for i in range(qtd):
+    nota = float(input(f"Digite a nota {i+1}: "))
+    soma += nota
+
+media = soma / qtd
 
 if media >= 6:
-    print(Nome_Aluno, "Você está aprovado! e sua nota foi :", round(media, 2))
+    print(Nome_Aluno,"Você está aprovado! e sua nota é: ", round(media,1))
 else:
-    print(Nome_Aluno, "Você está reprovado! e sua nota foi :", round(media, 2))
+    print(Nome_Aluno,"Você está reprovado! e sua nota é: ", round(media,1))
